0d1eba7f2ac9f7a224aaa7efc23ce61f2899149e
Jackie Elliott <je1283@nyu.edu>
2025-01-15 16:04:57 +0100

    added tonejs and synth logic

--- src/components/ConnectionState.vue
+++ src/components/ConnectionState.vue
@@ -3,23 +3,94 @@
   <div>
     <h2>Connected clients</h2>
     <ul>
-      <li v-for="client in clients" :key="client.id">{{ client.id }}</li>
+      <li v-for="client in clients" :key="client.signal">{{ client.signal }}</li>
     </ul>
   </div>
 </template>
 
 <script>
 import { state } from "@/socket";
+import * as Tone from "tone";
 
 export default {
   name: "ConnectionState",
+
+  data: function() {
+    return {
+      notes: ['A4', 'B4', 'C4']
+    }
+  }
+
+  // watch: {
+  //   // whenever clients connect, this function will run
+  //   clients(updatedClients) {
+  //     console.log('client list changed', updatedClients)
+
+  //     const diff = this.compareArrays(this.clients, updatedClients);
+  //     this.clients = [...this.clients, ...diff.added]
+
+  //     // still need to DELETE clients who are no longer with us: diff.removed
+
+  //     // here you can do tone.js stuff
+  //     for (var i=0; i<this.clients.length; i++) {
+  //       var c = this.clients[i];
+  //       // c.signal, c.ip, c.id
+
+  //       if (!c.synth) {
+  //         const note = this.notes[ Math.floor(Math.random(this.notes.length)) ]
+  //         const synth = new Tone.Synth().toDestination();
+  //         synth.triggerAttack(note); //starting pitch C4
+  //         // add to client object
+  //         c.synth = synth;
+  //       }
+  //       // set some other property of the synth tone based on c.signal?
+  //       c.synth.Destination.volume.value = c.signal;
+  //     }
+  //   }
+  // },
 
   computed: {
     connected() {
       return state.connected;
     },
     clients() {
-      return state.clients;
+      const diff = this.compareArrays(this.clients, updatedClients);
+      const updatedClients = [...this.clients, ...diff.added]
+
+      // still need to DELETE clients who are no longer with us: diff.removed
+
+      // here you can do tone.js stuff
+      for (var i=0; i<updatedClients.length; i++) {
+        var c = updatedClients[i];
+        // c.signal, c.ip, c.id
+
+        if (!c.synth) {
+          const note = this.notes[ Math.floor(Math.random(this.notes.length)) ]
+          const synth = new Tone.Synth().toDestination();
+          synth.triggerAttack(note); //starting pitch C4
+          // add to client object
+          c.synth = synth;
+        }
+        // set some other property of the synth tone based on c.signal?
+        c.synth.Destination.volume.value = c.signal;
+
+        return updatedClients;
+    }
+  },
+
+  methods: {
+    compareArrays(oldArray, newArray) {
+      const oldIPs = new Set(oldArray.map(item => item.ip))
+      const newIPs = new Set(newArray.map(item => item.ip))
+
+      const added = newArray.filter(item => !oldIPs.has(item.ip))
+      const removed = oldArray.filter(item => !newIPs.has(item.ip))
+
+      return {
+        added,
+        removed,
+        hasChanged: added.length > 0 || removed.length > 0
+      }
     }
   }
 }
--- src/socket.js
+++ src/socket.js
@@ -9,7 +9,7 @@ export const state = reactive({
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://kd2lfq.local:5000";
+const URL = "http://jelly.local:5000";
 
 export const socket = io(URL);
 
