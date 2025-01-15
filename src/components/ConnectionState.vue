29d5b80b31ed96590f137f4c5c4c57c5dc4dcc57
Jackie Elliott <je1283@nyu.edu>
2025-01-15 21:27:55 +0100

    fixing async audio

--- src/components/ConnectionState.vue
+++ src/components/ConnectionState.vue
@@ -66,15 +66,18 @@ export default {
 
   methods: {
     async startAudio() {
+      if (!this.toneStarted) {
       await Tone.start();
 
       //await Tone.resume();
       this.toneStarted = true;
       console.log("tone started");
-
+      }
+      if (!this.testSynth) {
       this.testSynth = new Tone.Synth().toDestination();
       this.testSynth.triggerAttack('A4', Tone.now());
-      c.synth.Destination.volume.value = -10;
+      }
+      //c.synth.Destination.volume.value = -10;
     },
     compareArrays(oldArray, newArray) {
       const oldIPs = new Set(oldArray.map(item => item.ip));
