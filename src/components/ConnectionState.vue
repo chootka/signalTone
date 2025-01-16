fcd402654401be315390c1d815b371d766cac4dc
Jackie Elliott <je1283@nyu.edu>
2025-01-16 16:08:34 +0100

    test

--- src/components/ConnectionState.vue
+++ src/components/ConnectionState.vue
@@ -1,12 +1,12 @@
 <template>
   <p>State: {{ connected }}</p>
   <div>
-    <button @click="startAudio()">Start Audio</button>
+    <button @click.prevent="startAudio()">Start Audio</button>
     <h2>Connected clients</h2>
     <ul>
       <li v-for="client in clients" :key="client.id">{{ client.signal }}</li>
     </ul>
-  </div> 
+  </div>
 </template>
 
 <script>
@@ -16,6 +16,7 @@ export default {
   name: "ConnectionState",
 
   data() {
+    
     return {
       testSynth: null,
       toneStarted: false,
@@ -31,20 +32,20 @@ export default {
       console.log("idx", idx);
       console.log("newClients", newClients)
       newClients.splice(idx, 1);
-      
+
       const diff = this.compareArrays(this.clients, newClients);
       this.clients = [...this.clients, ...diff.added]
 
       // still need to DELETE clients who are no longer with us: diff.removed
 
       // here you can do tone.js stuff
-      for (var i=0; i<this.clients.length; i++) {
+      for (var i = 0; i < this.clients.length; i++) {
         var c = this.clients[i];
         // c.signal, c.ip, c.id
         console.log("got a client signal", c.signal)
 
         if (!c.synth) {
-          const note = this.notes[ Math.floor(Math.random(this.notes.length)) ]
+          const note = this.notes[Math.floor(Math.random(this.notes.length))]
           const synth = new Tone.Synth().toDestination();
           if (this.toneStarted) synth.triggerAttack(note); //starting pitch C4
           // add to client object
@@ -66,32 +67,46 @@ export default {
 
   methods: {
     async startAudio() {
-      if (!this.toneStarted) {
-      await Tone.start();
+      console.log('starting audio')
+      try {
+        await Tone.start();
 
-      //await Tone.resume();
-      this.toneStarted = true;
-      console.log("tone started");
+        //await Tone.resume();
+        this.toneStarted = true;
+        console.log("tone started");
+        console.log("tone started for sure");
+
+        if (this.testSynth) {
+        this.testSynth = new Tone.Synth().toDestination();
+        console.log('prepping something')
+        this.testSynth.triggerAttack('A4', Tone.now());
+        console.log('played something')
+        //c.synth.Destination.volume.value = -10;
+        } else console.log('nada ')
+      } catch (error) {
+        console.log(error);
       }
-      if (!this.testSynth) {
-      this.testSynth = new Tone.Synth().toDestination();
-      this.testSynth.triggerAttack('A4', Tone.now());
-      }
-      //c.synth.Destination.volume.value = -10;
+
     },
-    compareArrays(oldArray, newArray) {
-      const oldIPs = new Set(oldArray.map(item => item.ip));
-      const newIPs = new Set(newArray.map(item => item.ip));
+    try {
+      compareArrays(oldArray, newArray) {
+        const oldIPs = new Set(oldArray.map(item => item.ip));
+        const newIPs = new Set(newArray.map(item => item.ip));
 
-      const added = newArray.filter(item => !oldIPs.has(item.ip));
-      const removed = oldArray.filter(item => !newIPs.has(item.ip));
+        const added = newArray.filter(item => !oldIPs.has(item.ip));
+        const removed = oldArray.filter(item => !newIPs.has(item.ip));
 
-      return {
-        added,
-        removed,
-        hasChanged: added.length > 0 || removed.length > 0
+        return {
+          added,
+          removed,
+          hasChanged: added.length > 0 || removed.length > 0
+        }
       }
+    } catch(error) {
+      console.log(error);
+
     }
+
   }
 }
 </script>
